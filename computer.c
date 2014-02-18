e452707b3db9b0a3ae33c792f46e5350ba63a4b5
Kenji Rikitake <kenji.rikitake@acm.org>
2014-02-18 09:33:55 +0900

    Update computer.c
    * Fix quadrant coordinate boundary in the course calculation

--- computer.c
+++ computer.c
@@ -190,10 +190,10 @@ computer(__unused int unused)
 			else
 			{
 				ix = getintpar("Quadrant");
-				if (ix < 0 || ix >= NSECTS)
+				if (ix < 0 || ix >= NQUADS)
 					break;
 				iy = getintpar("q-y");
-				if (iy < 0 || iy >= NSECTS)
+				if (iy < 0 || iy >= NQUADS)
 					break;
 				tqx = ix;
 				tqy = iy;
